9686804b743a332b22c231a04e4dd6b61f52832b
Fahad <fahad@mail.fi>
2024-12-01 15:39:57 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# **Kanye West Quotes**
+
+## About
+A `Kanye West Quotes` application is developed in Python using the Tkinter package for the user interface and the requests package for handling API interactions.
+
+It is a fun application that fetches a new quote by Kanye West every time the button is clicked. Quotes are fetched from `https://kanye.rest/`, a REST API for random Kanye West quotes.
+
+## Developed Using:
+[Python](https://www.python.org/)
+
+## Dependencies:
+- Python v3.6+
+- tkinter
+- requests
+- os
+
+
+## Contact Information:
+Fahad Chowdhury\
+[LinkedIn](https://www.linkedin.com/in/fahad-chowdhury-fi)\
+[GitHub](https://github.com/Fahad-Chowdhury)
+
+![Image Link](https://github.com/Fahad-Chowdhury/kanye-quotes/blob/main/app.JPG)
---
+++ main.py
@@ -0,0 +1,60 @@
+from tkinter import *
+import os
+import requests
+
+
+class KanyeQuotes():
+
+    def __init__(self):
+        self.window = None
+        self.canvas = None
+        self.quote_text = None
+        self.current_dir = os.path.dirname(os.path.abspath(__file__))
+        self.setup_ui()
+
+    def setup_ui(self):
+        """ Setup the User Interface. """
+        self._setup_window()
+        self._setup_canvas()
+        self._setup_button()
+
+    def _setup_window(self):
+        """ Setup the window. """
+        self.window = Tk()
+        self.window.title("Kanye Says...")
+        self.window.config(padx=50, pady=50)
+
+    def _setup_canvas(self):
+        """ Setup Canvas with background image. """
+        self.canvas = Canvas(width=300, height=414)
+        background_img_path = os.path.join(self.current_dir, "background.png")
+        self.background_img = PhotoImage(file=background_img_path)
+        self.canvas.create_image(150, 207, image=self.background_img)
+        self.quote_text = self.canvas.create_text(150, 207, text="Click button for Kanye Quotes",
+                                                  width=250, font=("Arial", 20, "bold"), fill="white")
+        self.canvas.grid(row=0, column=0)
+
+    def _setup_button(self):
+        """ Setup Button with Kanye West Image. """
+        kanye_img_path = os.path.join(self.current_dir, "kanye.png")
+        self.kanye_img = PhotoImage(file=kanye_img_path)
+        kanye_button = Button(image=self.kanye_img, highlightthickness=0, command=self.update_quote)
+        kanye_button.grid(row=1, column=0)
+
+    def update_quote(self):
+        ''' Update Kanye West quote in the canvas. '''
+        response = requests.get(url="https://api.kanye.rest")
+        # Raise exeception for response code other than 200
+        response.raise_for_status()
+        quote = response.json()["quote"]
+        self.canvas.itemconfig(self.quote_text, text=quote)
+
+
+def main():
+    """ Main method to execute Pomodoro app. """
+    kanye = KanyeQuotes()
+    kanye.window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
